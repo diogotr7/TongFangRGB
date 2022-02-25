e78de5871477f32e517adab29d2fcf53b9088a9c
Christos Pappas <chrispappas99@yahoo.gr>
2022-02-26 00:44:43 +0200

    Create test cases

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,26 +10,49 @@ namespace Test
     {
         private const byte ROWS = 6;
         private const byte COLUMNS = 21;
-        public static void Main()
+
+        public static void TestUpdateSingleKey(ITongFangKeyboard kb, Key k, byte r, byte g, byte b)
+        {
+            kb.SetKeyColor(k, r, g, b);
+            kb.Update();
+        }
+
+        public static void TestSetCoord(ITongFangKeyboard kb)
         {
             byte i = 0;
+            for (byte r = 0; r<ROWS; r++)
+            {
+                for (byte c = 0; c<COLUMNS; c++)
+                 {
+                    kb.SetCoordColor(r, c, 0, i, 0);
+                    i += 3; // Overflow on purpose
+                 }
+            }
+            kb.Update();
+        }
+
+        public static void TestSetKeyColor(ITongFangKeyboard kb)
+        {
+            byte i = 0;
+            foreach (var key in kb.Keys)
+            {
+                kb.SetKeyColor(key, i, 0, 0);
+                i += 3; // Overflow on purpose
+            }
+            kb.Update();
+        }
+
+        public static void Main()
+        {
             if (TongFindFinder.TryFind(100, Layout.ISO, out var kb))
             {
-                for (byte r = 0; r < ROWS; r++)
-                {
-                    for (byte c = 0; c < COLUMNS; c++)
-                    {
-                        kb.SetCoordColor(r, c, i, i, i);
-                        i++;
-                    }
-                }
-
-                foreach (var key in kb.Keys)
-                {
-                    kb.SetKeyColor(key, i++, i++, i++);
-                }
-
                 kb.Update();
+                foreach (var key in kb.Keys)
+                    TestUpdateSingleKey(kb, key, 255, 255, 255);
+                TestSetKeyColor(kb);
+                TestSetCoord(kb);
+
+                kb.Dispose();
             }
         }
     }
