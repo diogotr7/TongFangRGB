f151abeb95730b89da1e02066ef55c45d9018aab
Christos Pappas <chrispappas99@yahoo.gr>
2022-03-15 14:35:01 +0200

    Create Artemis Plugin

---
+++ ArtemisPlugin/RgbNetDeviceProvider/TongfangKeyboardRGBDevice.cs
@@ -0,0 +1,28 @@
+﻿using RGB.NET.Core;
+
+namespace RGB.NET.Devices.Tongfang
+{
+    public class TongfangKeyboardRGBDevice : AbstractRGBDevice<TongfangKeyboardDeviceInfo>
+    {
+        internal TongfangKeyboardRGBDevice(TongfangKeyboardDeviceInfo deviceInfo, IUpdateQueue updateQueue)
+            : base(deviceInfo, updateQueue)
+        {
+            InitializeLayout();
+        }
+
+        private void InitializeLayout()
+        {
+            int x = 0;
+            foreach (TongFang.Key key in DeviceInfo.TongFangKeyboard.Keys)
+            {
+                if (!TongfangLedMapping.TongfangLedMapping.Mapping.TryGetValue(key, out LedId ledId))
+                {
+                    continue;
+                }
+
+                AddLed(ledId, new Point(x, 0), new Size(19), key);
+                x += 20;
+            }
+        }
+    }
+}
---
+++ ArtemisPlugin/RgbNetDeviceProvider/TongfangUpdateQueue.cs
@@ -0,0 +1,25 @@
+﻿using RGB.NET.Core;
+using System;
+using TongFang;
+
+namespace RGB.NET.Devices.Tongfang
+{
+    public class TongfangUpdateQueue : UpdateQueue
+    {
+        private readonly ITongFangKeyboard _keyboard;
+
+        public TongfangUpdateQueue(IDeviceUpdateTrigger updateTrigger, ITongFangKeyboard keyboard) : base(updateTrigger)
+        {
+            _keyboard = keyboard;
+        }
+
+        protected override void Update(in ReadOnlySpan<(object key, Color color)> dataSet)
+        {
+            foreach ((object key, Color color) item in dataSet)
+            {
+                _keyboard.SetKeyColor((Key)item.key, item.color.GetR(), item.color.GetG(), item.color.GetB());
+            }
+            _keyboard.Update();
+        }
+    }
+}
