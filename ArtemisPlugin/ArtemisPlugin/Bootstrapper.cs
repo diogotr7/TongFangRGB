739447686d279ec27b79667cb7e4ad298238553a
Christos Pappas <chrispappas99@yahoo.gr>
2022-03-15 14:35:01 +0200

    Create Artemis Plugin

---
+++ ArtemisPlugin/ArtemisPlugin/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Artemis.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtemisPlugin
+{
+    public class Bootstrapper : PluginBootstrapper
+    {
+        public override void OnPluginLoaded(Plugin plugin)
+        {
+        }
+
+        public override void OnPluginEnabled(Plugin plugin)
+        {
+        }
+
+        public override void OnPluginDisabled(Plugin plugin)
+        {
+        }
+    }
+}
