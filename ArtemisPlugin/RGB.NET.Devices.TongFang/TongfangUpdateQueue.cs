b806412542c8dc560d0d481e28f42e4708636cab
Diogo Trindade <diogotr7@gmail.com>
2023-04-25 18:32:16 +0100

    clean up

---
+++ ArtemisPlugin/RGB.NET.Devices.TongFang/TongfangUpdateQueue.cs
@@ -0,0 +1,26 @@
+﻿using RGB.NET.Core;
+using System;
+using TongFang;
+
+namespace RGB.NET.Devices.Tongfang;
+
+public class TongfangUpdateQueue : UpdateQueue
+{
+    private readonly ITongFangKeyboard _keyboard;
+
+    public TongfangUpdateQueue(IDeviceUpdateTrigger updateTrigger, ITongFangKeyboard keyboard) : base(updateTrigger)
+    {
+        _keyboard = keyboard;
+    }
+
+    protected override bool Update(in ReadOnlySpan<(object key, Color color)> dataSet)
+    {
+        foreach ((object key, Color color) in dataSet)
+        {
+            (byte row, byte column) = ((byte, byte))key;
+            _keyboard.SetCoordColor(row, column, color.GetR(), color.GetG(), color.GetB());
+        }
+
+        return _keyboard.Update();
+    }
+}
