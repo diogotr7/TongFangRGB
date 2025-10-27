9f8e215157a6a2dd6f54adc5fbcf473969c044b7
Richard Corbishley <riccorbypro@gmail.com>
2025-10-27 08:44:13 +0200

    Refactor `TongFindFinder` method call and clean up keyboard color logic. Update launch settings path.

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using TongFang;
@@ -10,20 +11,84 @@ namespace Test
     {
         public static void Main()
         {
-            if (TongFindFinder.TryFind(out var kb))
+            if (TongFindFinder.TryFindKeyboard(out var kb))
             {
                 kb.SetColor(0, 0, 0);
                 kb.Update();
 
+                //white
                 for (byte row = 0; row < kb.Rows; row++)
                 {
                     for (byte col = 0; col < kb.Columns; col++)
                     {
                         kb.SetCoordColor(row, col, 255, 255, 255);
                         kb.Update();
-                        Thread.Sleep(50);
                     }
                 }
+                
+                // //pink
+                // for (byte row = 0; row < kb.Rows; row++)
+                // {
+                //     for (byte col = 0; col < kb.Columns; col++)
+                //     {
+                //         kb.SetCoordColor(row, col, 255, 0, 255);
+                //         kb.Update();
+                //     }
+                // }
+                //
+                // //red
+                // for (byte row = 0; row < kb.Rows; row++)
+                // {
+                //     for (byte col = 0; col < kb.Columns; col++)
+                //     {
+                //         kb.SetCoordColor(row, col, 255, 0, 0);
+                //         kb.Update();
+                //     }
+                // }
+                //
+                // //yellow
+                // for (byte row = 0; row < kb.Rows; row++)
+                // {
+                //     for (byte col = 0; col < kb.Columns; col++)
+                //     {
+                //         kb.SetCoordColor(row, col, 255, 255, 0);
+                //         kb.Update();
+                //     }
+                // }
+                //
+                // //green
+                // for (byte row = 0; row < kb.Rows; row++)
+                // {
+                //     for (byte col = 0; col < kb.Columns; col++)
+                //     {
+                //         kb.SetCoordColor(row, col, 0, 255, 0);
+                //         kb.Update();
+                //     }
+                // }
+                //
+                // //teal
+                // for (byte row = 0; row < kb.Rows; row++)
+                // {
+                //     for (byte col = 0; col < kb.Columns; col++)
+                //     {
+                //         kb.SetCoordColor(row, col, 0, 255, 255);
+                //         kb.Update();
+                //     }
+                // }
+                //
+                // //blue
+                // for (byte row = 0; row < kb.Rows; row++)
+                // {
+                //     for (byte col = 0; col < kb.Columns; col++)
+                //     {
+                //         kb.SetCoordColor(row, col, 0, 0, 255);
+                //         kb.Update();
+                //     }
+                // }
+                
+                Thread.Sleep(1000);
+                kb.SetColor(0, 0, 0);
+                kb.Update();
 
                 kb.Dispose();
             }
